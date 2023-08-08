bdd6e874a8e66d1408edd0b677509d273f780e5e
TianaD <tldavis99@yahoo.com>
2023-08-08 11:01:59 -0500

    fixing branch and pull and fetch and sync

--- c-sharp/CodeChallenge10/StackAndQueue.cs
+++ c-sharp/CodeChallenge10/StackAndQueue.cs
@@ -1,16 +1,33 @@
 namespace CodeChallenges
 {
-    public class Node
-    {
+  public class Node
+  {
+    public int Value { get; set; }
+    public Node Next { get; set; }
 
+  }
+
+  public class Stack
+  {
+    //Create a Stack class that has a top property. It creates an empty Stack when instantiated
+    private Node Top { get; set; }
+    public Stack(Node node)
+    {
+      Top = null; //This object should be aware of a default empty value assigned to top when the stack is created.
+    }
+    // push class, Arguments: value
+    public void Push( int value )
+    {
+      // adds a new node with that value to the top of the stack with an O(1) Time performance.
+      Node newNode = new Node(value);
+      newNode.Next = Top;
+      Top = newNode;
     }
 
-    public class Stack
-    {
+  }
 
-    }
+public class Queue
+{
 
-    public class Queue
-    {
-    }
+}
 }
