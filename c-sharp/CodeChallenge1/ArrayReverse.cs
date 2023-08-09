1ffdd243c3b67ae5ff14fe31944d19a448de83b1
TianaD <tldavis99@yahoo.com>
2023-08-09 09:32:44 -0500

    Challenge01

--- c-sharp/CodeChallenge1/ArrayReverse.cs
+++ c-sharp/CodeChallenge1/ArrayReverse.cs
@@ -1,12 +1,21 @@
 using System.Collections;
+
 namespace CodeChallenges
 {
-    public class CodeChallenge1
+  public class CodeChallenge1
+  {
+    public static int[] ReverseArray(int[] array)
     {
-        public static int[] ReverseArray(int[] array)
-        {
+      int[] tempArray = new int[array.Length];
+      int tempArrayLength = tempArray.Length;
 
-            return array;
-        }
+      //reverse array
+      for (int i = array.Length; i > 0; i--)
+      {
+        tempArray[tempArrayLength - i] = array[i - 1];
+        Console.WriteLine(array[i]);
+      }
+      return tempArray;
     }
+  }
 }
