b0db5ca9a553a013c832bf854973094b43e0a937
TianaD <tldavis99@yahoo.com>
2023-08-09 09:30:31 -0500

    Challenge01

--- c-sharp/CodeChallenge2/ArrayInsertShift.cs
+++ c-sharp/CodeChallenge2/ArrayInsertShift.cs
@@ -1,11 +1,34 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class CodeChallenge2
+  public class CodeChallenge2
+  {
+    public static int[] InsertShiftArray(int[] array, int value)
     {
-        public static int[] InsertShiftArray(int[] array, int value)
+      int[] resultArray = new int[array.Length + 1];
+
+      int shiftIndex = array.Length / 2;
+
+      for (int i = 0; i < resultArray.Length; i++)
+      {
+        if (i < shiftIndex)
         {
-            //insert the value into the middle of the array
-            return array;
+          resultArray[i] = array[i];
+
         }
+        else if (i == shiftIndex)
+        {
+          resultArray[shiftIndex] = value;
+
+        }
+        else
+        {
+          resultArray[i] = array[i - 1];
+
+        }
+      }
+
+      return resultArray;
     }
+
+  }
 }
