8f5d55b766884ca9b9a2984705acd3ed3b59fe8f
TianaD <tldavis99@yahoo.com>
2023-08-09 10:01:19 -0500

    Added comments to Challenge04

--- c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
+++ c-sharp/CodeChallenge4/FibonacciAndRowSums.cs
@@ -1,8 +1,8 @@
 namespace CodeChallenges 
 {
-  //
     public class CodeChallenge4
     {
+        // This method returns the nth number in the Fibonacci sequence.
         public static int GetNthNumberInFibonacciSequence(int nthNumberInSequence)
         {
             int result = 0;
@@ -10,6 +10,7 @@ namespace CodeChallenges
             return result;
         }
 
+        // This method returns an array of integers representing the sum of each row in a matrix.
         public static int[] RowSums(int[][] matrix)
         {
             int[] rowSums = new int[matrix[0].Length];
