1b2c9f56c938ec94a08a715a062d60ff9ce2fd46
TianaD <tldavis99@yahoo.com>
2023-08-14 15:05:01 -0500

    Added 8 to 9

--- c-sharp/CodeChallenge8/LinkedListZip.cs
+++ c-sharp/CodeChallenge8/LinkedListZip.cs
@@ -6,6 +6,26 @@ namespace CodeChallenges
     public static LinkedListKth ZipLists(LinkedListKth list1, LinkedListKth list2)
     {
       LinkedListKth result = new LinkedListKth();
+      Node currentNode1 = list1.Head;
+      Node currentNode2 = list2.Head;
+
+      while (currentNode1 != null || currentNode2 != null)
+      {
+        if (currentNode1 != null)
+        {
+          result.Append(currentNode1.Value);
+          currentNode1 = currentNode1.Next;
+        }
+
+        if (currentNode2 != null)
+        {
+          result.Append(currentNode2.Value);
+          currentNode2 = currentNode2.Next;
+        }
+      }
+
+
+
       return result;
     }
   }
--- c-sharp/CodeChallenge9/SinglyLinkedList.cs
+++ c-sharp/CodeChallenge9/SinglyLinkedList.cs
@@ -4,13 +4,42 @@ namespace CodeChallenges
   {
     public static LinkedListKth ReverseLinkedList(LinkedListKth list)
     {
+      if (list.Head == null)
+      {
+        throw new InvalidOperationException("Cannot reverse an empty list");
+      }
+
+      Node current = list.Head;
+      Node prev = null;
+      Node next;
+
+      while (current != null)
+      {
+        next = current.Next;
+        current.Next = prev;
+        prev = current;
+        current = next;
+
+      }
+      list.Head = prev;
       return list;
     }
 
     public static bool IsPalindrome(LinkedListKth list)
     {
-      bool isPalindrome = false;
-      return isPalindrome;
+      Node end = list.Tail;
+      Node current = list.Head;
+
+      while (current != null)
+      {
+        if (end.Value != current.Value)
+        {
+          return false;
+        }
+        current = current.Next;
+        end = end.Prev;
+      }
+      return true;
     }
   }
 }
