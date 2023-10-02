3f9e24e59a282b56c28d3b610ab6673194baa101
TianaD <tldavis99@yahoo.com>
2023-10-02 12:33:19 -0500

    challenge 11 complete

--- c-sharp/CodeChallenge11/StackQueuePsuedo.cs
+++ c-sharp/CodeChallenge11/StackQueuePsuedo.cs
@@ -1,8 +1,43 @@
+using System;
 using Xunit;
 namespace CodeChallenges
 {
   public class StackQueuePsuedo
   {
+    private Stack<int> enqueueStack; // Stack for enqueue operation
+    private Stack<int> dequeueStack; // Stack for dequeue operation
 
+    public StackQueuePsuedo()
+    {
+      enqueueStack = new Stack<int>();
+      dequeueStack = new Stack<int>();
+    }
+
+    public void Enqueue(int value)
+    {
+      // To enqueue, simply push the value onto the enqueueStack
+      enqueueStack.Push(value);
+    }
+
+    public int Dequeue()
+    {
+      // If both stacks are empty, throw an exception as the queue is empty
+      if (enqueueStack.Count == 0 && dequeueStack.Count == 0)
+      {
+        throw new InvalidOperationException("PseudoQueue is empty.");
+      }
+
+      // If dequeueStack is empty, transfer all elements from enqueueStack to dequeueStack
+      if (dequeueStack.Count == 0)
+      {
+        while (enqueueStack.Count > 0)
+        {
+          dequeueStack.Push(enqueueStack.Pop());
+        }
+      }
+
+      // Pop the top element from dequeueStack (front of the "queue")
+      return dequeueStack.Pop();
+    }
   }
 }
