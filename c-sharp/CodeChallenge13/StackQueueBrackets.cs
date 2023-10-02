05f79d4b377e2efa3c940ce5283871f22feaaad6
TianaD <tldavis99@yahoo.com>
2023-10-02 12:38:06 -0500

    challenge 13 complete

--- c-sharp/CodeChallenge13/StackQueueBrackets.cs
+++ c-sharp/CodeChallenge13/StackQueueBrackets.cs
@@ -1,10 +1,38 @@
-namespace CodeChallenges 
+using System.Collections;
+namespace CodeChallenges
 {
-    public class CodeChallenge13
+  public class CodeChallenge13
+  {
+    public static bool ValidateBrackets(string syntax)
     {
-        public static bool ValidateBrackets(string syntax)
+      Stack Brackets = new Stack();
+
+      foreach (char c in syntax)
+      {
+        if (c == '[' || c == '(' || c == '{')
         {
-            return false;
+          Brackets.Push(c);
         }
+        else if (c == ']' || c == ')' || c == '}')
+        {
+          if (Brackets.Count == 0)
+          {
+            return false; // Unmatched closing bracket
+          }
+
+          char top = (char)Brackets.Peek();
+          if ((c == ']' && top == '[') || (c == ')' && top == '(') || (c == '}' && top == '{'))
+          {
+            Brackets.Pop();
+          }
+          else
+          {
+            return false; // Mismatched closing bracket
+          }
+        }
+      }
+
+      return Brackets.Count == 0; // All brackets should be matched
     }
+  }
 }
